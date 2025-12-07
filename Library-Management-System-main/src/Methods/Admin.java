d8274e991f09465fad394fd3e8d9b67ddee07b33
jamesdee0904 <24-37104@g.batstate-u.edu.ph>
2025-12-07 11:23:57 +0800

    Update Admin.java

--- Library-Management-System-main/src/Methods/Admin.java
+++ Library-Management-System-main/src/Methods/Admin.java
@@ -1,12 +1,12 @@
 package Methods;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Admin extends Account{
+
     Scanner sc = new Scanner(System.in);
     private String admin_email = "Admin123";
     private String password = "AdminPassword";
@@ -15,6 +15,8 @@ public class Admin extends Account{
 
     public Admin(Student student) {
         this.student = student;
+        book();
+        accounts();
     }
     @Override
     public void login() {
@@ -24,7 +26,7 @@ public class Admin extends Account{
         String pass = sc.nextLine();
         if (email.equals(admin_email) && pass.equals(password)) {
             while (true) {
-                System.out.println("\t[1] Student Login\n\t[2] Borrowed Book\n\t[3] Returned Book\n\t[4] Add Account\n\t[5] Exit");
+                System.out.println("\t[1] Student Login\n\t[2] Borrowed Book\n\t[3] Returned Book\n\t[4] Add Account\n\t[5] Book Management\n\t[6] Exit");
                 System.out.print("\t\tEnter number: ");
                 int num = sc.nextInt();
                 if (num == 1) {
@@ -34,10 +36,12 @@ public class Admin extends Account{
                 } else if (num == 3) {
                     returnedBook();
                 } else if (num == 4) {
-                    //add account
+                    accountMenu();
                 } else if(num == 5){
+                    bookMenu();
+                } else if(num == 6){
                     break;
-                }else {
+                } else {
                     System.out.println("Invalid input, re-try.");
                 }
             }
@@ -103,10 +107,183 @@ public class Admin extends Account{
         book.put(6, "Discrete Mathematics");
     }
     public void showBooks(){
+        System.out.println(" - ".repeat(22));
         for (Map.Entry<Integer, String> entry : book.entrySet()){
             Integer id = entry.getKey();
             String title = entry.getValue();
             System.out.println("\tID: " + id + " Title: " + title);
         }
     }
+
+public void bookMenu() {
+    while (true) {
+        System.out.println(" - ".repeat(22));
+        System.out.println("\t[1] Show Books");
+        System.out.println("\t[2] Add Book");
+        System.out.println("\t[3] Edit Book");
+        System.out.println("\t[4] Delete Book");
+        System.out.println("\t[5] Back");
+        System.out.print("\t\tEnter number: ");
+        int select = sc.nextInt();
+
+        if (select == 1) {
+            showBooks();
+        } else if (select == 2) {
+            addBook();
+        } else if (select == 3) {
+            editBook();
+        } else if (select == 4) {
+            deleteBook();
+        } else if (select == 5) {
+            break;
+        } else {
+            System.out.println(" - ".repeat(22));
+            System.out.println("\t\tInvalid input.");
+        }
+    }
 }
+
+public void addBook() {
+    System.out.println(" - ".repeat(22));
+    System.out.print("\tEnter New Book ID: ");
+    int id = sc.nextInt();
+    sc.nextLine();
+
+    if (book.containsKey(id)) {
+        System.out.println(" - ".repeat(22));
+        System.out.println("\tBook ID already exists. Please use another ID.");
+        return;  // stop here
+    }
+
+    System.out.print("\tEnter Book Title: ");
+    String title = sc.nextLine();
+
+    book.put(id, title);
+    System.out.println(" - ".repeat(22));
+    System.out.println("\tBook Added Successfully!");
+}
+
+public void editBook() {
+    System.out.println(" - ".repeat(22));
+    System.out.print("\tEnter Book ID to Edit: ");
+    int id = sc.nextInt();
+    sc.nextLine();
+
+    if (book.containsKey(id)) {
+        System.out.print("\tEnter New Title: ");
+        String newTitle = sc.nextLine();
+        book.put(id, newTitle);
+        System.out.println("\tBook Updated Successfully!");
+    } else {
+        System.out.println(" - ".repeat(22));
+        System.out.println("\tBook ID Not Found.");
+    }
+}
+
+public void deleteBook() {
+    System.out.println(" - ".repeat(22));
+    System.out.print("\tEnter Book ID to Delete: ");
+    int id = sc.nextInt();
+
+    if (book.containsKey(id)) {
+        book.remove(id);
+        System.out.println("\tBook Deleted Successfully!");
+    } else {
+        System.out.println(" - ".repeat(22));
+        System.out.println("\tBook ID Not Found.");
+    }
+}
+
+
+@Override
+public void accountMenu() {
+    while (true) {
+        System.out.println(" - ".repeat(22));
+        System.out.println("\t[1] Show Accounts");
+        System.out.println("\t[2] Add Account");
+        System.out.println("\t[3] Edit Account and Password");
+        System.out.println("\t[4] Delete Account");
+        System.out.println("\t[5] Back");
+        System.out.print("\tEnter number: ");
+
+        int option = sc.nextInt();
+        sc.nextLine();
+
+        if (option == 1) {
+            showAccounts();
+        } else if (option == 2) {
+            addAccount();
+        } else if (option == 3) {
+            editAccount();
+        } else if (option == 4) {
+            deleteAccount();
+        } else if (option == 5) {
+            break;
+        } else {
+            System.out.println("\tInvalid Input.");
+        }
+    }
+}
+
+public void showAccounts() {
+    if (account.isEmpty()) {
+        System.out.println("\tNo accounts available.");
+        return;
+    }
+
+    System.out.println(" - ".repeat(22));
+    for (String email : account.keySet()) {
+        System.out.println("\tEmail: " + email + " | Password: " + account.get(email));
+    }
+}
+
+public void addAccount() {
+    System.out.println(" - ".repeat(22));
+    System.out.print("\tEnter Email: ");
+    String email = sc.nextLine();
+
+    if (account.containsKey(email)) {
+        System.out.println("\tAccount already exists.");
+        return;
+    }
+
+    System.out.print("\tEnter Password: ");
+    String pass = sc.nextLine();
+
+    account.put(email, pass);
+    System.out.println("\tAccount Added Successfully!");
+}
+
+public void editAccount() {
+    System.out.println(" - ".repeat(22));
+    System.out.print("\tEnter Email to Edit: ");
+    String email = sc.nextLine();
+
+    if (!account.containsKey(email)) {
+        System.out.println("\tAccount not found.");
+        return;
+    }
+
+    System.out.print("\tEnter New Password: ");
+    String newPass = sc.nextLine();
+
+    account.put(email, newPass);
+    System.out.println("\tPassword Updated!");
+}
+
+public void deleteAccount() {
+    System.out.println(" - ".repeat(22));
+    System.out.print("\tEnter Email to Delete: ");
+    String email = sc.nextLine();
+
+    if (!account.containsKey(email)) {
+        System.out.println("\tAccount not found.");
+        return;
+    }
+
+    account.remove(email);
+    System.out.println("\tAccount Deleted Successfully!");
+    }
+}
+
+
